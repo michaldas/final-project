0bf891869c9dbebce5939127af36811772071da1
Marina Mezvinsky <marmez08@gmail.com>
2017-11-29 13:04:38 +0200

    invited worked

--- meeting/invitedCtrl.js
+++ meeting/invitedCtrl.js
@@ -3,7 +3,7 @@ app.controller("invitedCtrl", function($scope, Invited){
           
     $scope.inviteds = [];
     $scope.addInvited = function(){
-     $scope.inviteds.push(new Invited( $scope.name, $scope.amount));
+     $scope.inviteds.push(new Invited( $scope.name, $scope.amount, completed = false));
      $scope.name = "";
      $scope.amount = "";
      
