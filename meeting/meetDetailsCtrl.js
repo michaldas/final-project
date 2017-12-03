12f79ec001bcfc034524b13cedb0c998bd06d342
Michal Dasberg <michaldasb@gmail.com>
2017-12-04 00:00:30 +0200

    details: add inviteds

--- meeting/meetDetailsCtrl.js
+++ meeting/meetDetailsCtrl.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-app.controller("meetDetailsCtrl", function ($scope, meetings, activeUser,  $routeParams, Meet, $http, Task, Cost) {
-=======
-app.controller("meetDetailsCtrl", function ($scope, meetings, activeUser,  $routeParams, Meet, $http, Task, $location) {
->>>>>>> 99393ceb6c906a0b85e3eeb6457a0edfb8472a18
+app.controller("meetDetailsCtrl", function ($scope, meetings, activeUser,  $routeParams, Meet, $http, Task, $location, Cost, Invited ) {
  //   $scope.meetings = []; 
     
     $scope.meet = meetings.get($routeParams.meetIndex);
 
-
+//Tasks
 
    $scope.addTask = function() {
         var task = new Task(item = $scope.formTaskItem, amount = $scope.formTaskAmount, completed = false);
@@ -18,8 +14,6 @@ app.controller("meetDetailsCtrl", function ($scope, meetings, activeUser,  $rout
     $scope.removeTask = function() {
         $scope.meet.tasks.splice(this.$index, 1); 
     }
-
-    
 
     $scope.clearCompleted = function() {
         var oldList = $scope.meet.tasks;
@@ -35,8 +29,40 @@ app.controller("meetDetailsCtrl", function ($scope, meetings, activeUser,  $rout
           if (x.completed) $scope.meet.tasks.push(x);
         });
       }
-<<<<<<< HEAD
-      
+//invited
+$scope.addInvited = function() {
+    var invited = new Invited(name = $scope.formInvitedName, amount = $scope.formInvitedAmount, completed = false);
+    $scope.meet.addInvited(invited);
+    $scope.formInvitedName = "";
+    $scope.formInvitedAmount = "";
+}
+$scope.removeInvited = function() {
+    $scope.meet.inviteds.splice(this.$index, 1); 
+}
+
+$scope.clearCompleted = function() {
+    var oldList = $scope.meet.inviteds;
+    $scope.meet.inviteds = [];
+    angular.forEach(oldList, function(x) {
+      if (!x.completed) $scope.meet.inviteds.push(x);
+    });
+  }
+   $scope.showCompleted = function() {
+    var oldList = $scope.meet.inviteds;
+    $scope.meet.inviteds = [];
+    angular.forEach(oldList, function(x) {
+      if (x.completed) $scope.meet.inviteds.push(x);
+    });
+  }
+
+   $scope.sumInvited = function() {
+    var sum = 0;
+    for( var i = 0; i<$scope.meet.inviteds.length; i++){
+      sum = sum + parseInt ($scope.meet.inviteds[i].amount);
+    }
+    return sum ;  
+    };
+ //costs     
       $scope.addCost = function addCost() {
        var cost = new cost(costelement = $scope.formCostelement, amount = $scope.formAmount) ;
        $scope.meet.addCost(cost);
@@ -46,14 +72,19 @@ app.controller("meetDetailsCtrl", function ($scope, meetings, activeUser,  $rout
    $scope.removeCost = function() {
        $scope.meet.costs.splice(this.$index, 1); 
    }
+   $scope.sumCost = function() {
+    var sum = 0;
+    for( var i = 0; i<$scope.meet.costs.length; i++){
+      sum = sum + parseInt ($scope.meet.costs[i].amount);
+    }
+    return sum ;  
+    };
 
    
-     
-=======
+//Update Button     
       $scope.update = function(){
         $location.path("/meet");
       }
->>>>>>> 99393ceb6c906a0b85e3eeb6457a0edfb8472a18
  
     
 });
