14e3b930cb833546dc727a6c7940abfa38a49c48
Marina Mezvinsky <marmez08@gmail.com>
2017-12-03 21:18:13 +0200

    add meetings to gallery

--- meeting/meetingsGalleryCtrl.js
+++ meeting/meetingsGalleryCtrl.js
@@ -4,19 +4,27 @@ app.controller("meetingsGalleryCtrl", function ($scope, meetings, $http, $log, $
         
     
    // console.log($scope.meetings);*/
+   if (meetings.getAll().length === 0){
+    $scope.meetings = []  ; 
    $http.get("data/meet.json").then(function mySuccess(response) {
     console.log("success open file meet.json");
     meetings.setMeets(response.data);                          
     $scope.meetings = meetings.getAll(); 
     console.log($scope.meetings);                                                        
-},  function myError(response) {
-    console.log("error open file meet.json");
-});
+});  
+ } else {
+    $scope.meetings = meetings.getAll();
+   }
 
+   console.log($scope.meetings);  
  $scope.openDetails = function (index) {       
            
         $location.path("/meet/" + index)
     }
            
 
-}) 
+}); 
+
+//function myError(response) {
+ //   console.log("error open file meet.json");
+//});
